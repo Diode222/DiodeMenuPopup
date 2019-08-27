da1962c8a62ae27bcdb902d502837be57375d4cf
Diode222 <54074254+Diode222@users.noreply.github.com>
2019-08-27 23:29:42 +0800

    增加屏幕变暗功能

---
+++ README.md
@@ -0,0 +1,2 @@
+# DiodeMenuPopup
+主要为了封装背景变暗。
--- app/src/main/java/com/erjiguan/diodemenupopup/DiodeMenuPopup.java
+++ app/src/main/java/com/erjiguan/diodemenupopup/DiodeMenuPopup.java
@@ -1,15 +1,32 @@
 package com.erjiguan.diodemenupopup;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.PopupMenu;
 
-// TODO 主要是为了封装背景变暗，制定viewgroup变暗的功能
 public class DiodeMenuPopup extends PopupMenu {
 
     public DiodeMenuPopup(@NonNull Context context, @NonNull View anchor) {
         super(context, anchor);
+    }
+
+    public void dimBackground(final float fromDimValue, final float toDimValue, final Window window) {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(fromDimValue, toDimValue);
+        valueAnimator.setDuration(200);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                WindowManager.LayoutParams params = window.getAttributes();
+                params.alpha = (Float) animation.getAnimatedValue();
+                window.setAttributes(params);
+            }
+        });
+
+        valueAnimator.start();
     }
 }
